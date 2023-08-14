47330c1c19a172a89908a3ec1977282727dd223a
Jose Jorge Nguiraze Junior <jjnguirazej@gmail.com>
2023-08-14 20:43:00 +0200

    navbar inicial

---
+++ todo-app/src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <nav>
+
+        <ul class="navbar-nav ms-auto">
+            <li class="nav-item">
+
+                <a href="#" class="btn btn-outline-secondary ms-2">Login</a>
+
+            </li>
+
+            <li class="nav-item">
+
+                <a href="#" class="btn btn-danger ms-2">Register</a>
+
+            </li>
+
+            <li class="nav-item">
+
+                <a href="#" class="btn btn-outline-secondary ms-2">Logout</a>
+
+            </li>
+        </ul>
+    </nav>
+
+</template>
